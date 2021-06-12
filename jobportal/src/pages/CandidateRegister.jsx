d9f04f1abe14ee9c0a82b72f84ef5674bfb554a3
LakhanBhagnani <lakhan.bhagnani@ves.ac.in>
2021-06-12 22:41:39 +0530

    Updated JS validation and Formatted Form
    Updated JS validation and Formatted Form

--- jobportal/src/pages/CandidateRegister.jsx
+++ jobportal/src/pages/CandidateRegister.jsx
@@ -53,7 +53,56 @@ const CandidateRegister = () => {
 			});
 	};
 
-	
+	const checkpassword=(pwd)=>{
+		if(pwd==Password)
+			{setPassword(pwd)
+				document.getElementById('passwordSpan').innerHTML=""}
+		else
+		{
+			debugger;
+			document.getElementById('passwordSpan').innerHTML="Password doesn't match";
+			document.getElementById('passwordSpan').classList.add("myspans");
+		}
+	}
+	const checkConstraints=(passwd)=>{
+		let ele=document.getElementById("passwordConstraints");
+		let lowerCaseLetters = /[a-z]/g;
+		let upperCaseLetters=/[A-Z]/g;
+		let Numbers=/[0-9]/g;
+		let specialChars=/[$,@,#]/g
+		let isValid= true;
+		if(passwd.length<8){
+			ele.innerHTML="length must be greater than 8";
+			isValid=false
+		}else
+		if(passwd.match(lowerCaseLetters)==null){
+			ele.innerHTML="Password should contains small letters"
+			isValid=false
+		}else
+		if(passwd.match(upperCaseLetters)==null){
+			ele.innerHTML="Password should contains Capitals letters"
+			isValid=false
+		}else
+		 if(passwd.match(Numbers)==null){
+			ele.innerHTML="Password should contains Numbers"
+			isValid=false
+		}else
+		if(passwd.match(specialChars)==null){
+			ele.innerHTML="Password should contains special like #,@,$"
+			isValid=false
+		}else
+		if(isValid)
+		{
+			ele.innerHTML=""
+			setPassword(passwd);
+		}
+		else{
+			ele.classList.add("myspans");
+		}
+		
+		
+
+	}
 
 	return (
 		<>
@@ -67,10 +116,10 @@ const CandidateRegister = () => {
 							type='email'
 							className='form-control'
 							id='floatingInput'
-							placeholder='name@example.com'
+							placeholder='Email-ID'
 							onChange={(e) => setEmail(e.target.value)}
 						/>
-						<label for='floatingInput'>Email</label>
+
 					</div>
 					<div className='form-floating'>
 						<input
@@ -80,108 +129,114 @@ const CandidateRegister = () => {
 							placeholder='First Name'
 							onChange={(e) => setFname(e.target.value)}
 						/>
-						<label for='floatingPassword'>First Name</label>
+
 					</div>
 					<div className='form-floating'>
 						<input
 							type='text'
 							className='form-control'
 							id='floatingPassword'
-							placeholder='Password'
+							placeholder='Last Name'
 							onChange={(e) => setLName(e.target.value)}
 						/>
-						<label for='floatingPassword'>Last Name</label>
+			
 					</div>
+					
 					<div className='form-floating'>
 						<input
 							type='text'
 							className='form-control'
 							id='floatingPassword'
-							placeholder='Password'
-							onChange={(e) => setUsername(e.target.value)}
-						/>
-						<label for='floatingPassword'>Username</label>
-					</div>
-					<div className='form-floating'>
-						<input
-							type='password'
-							className='form-control'
-							id='floatingPassword'
-							placeholder='Password'
-							onChange={(e) => setPassword(e.target.value)}
-						/>
-						<label for='floatingPassword'>Password</label>
-					</div>
-					<div className='form-floating'>
-						<input
-							type='password'
-							className='form-control'
-							id='floatingPassword'
-							placeholder='Password'
-							onChange={(e) => setCPassword(e.target.value)}
-						/>
-						<label for='floatingPassword'>Confirm Password</label>
-					</div>
-					<div className='form-floating'>
-						<input
-							type='date'
-							className='form-control'
-							id='floatingPassword'
-							placeholder='Password'
+							placeholder='Date of Birth'
+							onFocus={(e)=>e.target.type='date'}
 							onChange={(e) => setDOB(e.target.value)}
 						/>
-						<label for='floatingPassword'>Date of Birth</label>
+		
 					</div>
 					<div className='form-floating'>
 						<input
 							type='text'
 							className='form-control'
 							id='floatingPassword'
-							placeholder='Password'
+							placeholder='Qualification'
 							onChange={(e) => setQualification(e.target.value)}
 						/>
-						<label for='floatingPassword'>Qualification</label>
+			
 					</div>
 					<div className='form-floating'>
 						<input
-							type='date'
+							type='text'
 							className='form-control'
 							id='floatingPassword'
-							placeholder='Password'
+							placeholder='Passing Date'
+							onFocus={(e)=>e.target.type='date'}
 							onChange={(e) => setPassingDate(e.target.value)}
 						/>
-						<label for='floatingPassword'>Passing date</label>
+					
 					</div>
 					<div className='form-floating'>
 						<input
 							type='text'
 							className='form-control'
 							id='floatingPassword'
-							placeholder='Password'
+							placeholder='Current City'
 							onChange={(e) => setCurrentCity(e.target.value)}
 						/>
-						<label for='floatingPassword'>Current City</label>
+				
 					</div>
 					<div className='form-floating'>
 						<input
 							type='text'
 							className='form-control'
 							id='floatingPassword'
-							placeholder='Password'
+							placeholder='Skills(comma separated)'
 							onChange={(e) => setSkills(e.target.value)}
 						/>
-						<label for='floatingPassword'>Skills</label>
+				
 					</div>
 					<div className='form-floating'>
 						<input
 							type='number'
 							className='form-control'
 							id='floatingPassword'
-							placeholder='Password'
+							placeholder='Experience in Years'
 							min='0'
 							onChange={(e) => setExperience(e.target.value)}
 						/>
-						<label for='floatingPassword'>Experience</label>
+					
+					</div>
+					<div className='form-floating'>
+						<input
+							type='text'
+							className='form-control'
+							id='floatingPassword'
+							placeholder='Username'
+							onChange={(e) => setUsername(e.target.value)}
+							required
+						/>
+	
+					</div>
+					<div className='form-floating'>
+						<input
+							type='password'
+							className='form-control'
+							id='floatingPassword'
+							placeholder='Password'
+							pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+							onChange={(e) => checkConstraints(e.target.value)}
+							required
+						/><span class="myspans" id="passwordConstraints"></span>
+					</div>
+					<div className='form-floating'>
+						<input
+							type='password'
+							className='form-control'
+							id='floatingPassword'
+							placeholder='Confirm Password'
+							onChange={(e) => checkpassword(e.target.value)}
+							required
+						/>
+						<span  id="passwordSpan"></span>
 					</div>
 					<button className='w-100 btn btn-lg btn-primary' type='submit'>
 						Register
