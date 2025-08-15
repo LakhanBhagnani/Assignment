5b02d53448921295922c2881eeaeba8832f9cdfa
lakhanbhagnani <lakhan.bhagnani>
2025-08-15 05:58:19 +0530

    modifiedNavbar

--- jobportal/src/component/NavBar.jsx
+++ jobportal/src/component/NavBar.jsx
@@ -1,27 +1,27 @@
-import React, { Component, useState } from "react";
-import { Redirect } from "react-router-dom";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+// eslint-disable-next-line
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 const NavBar = () => {
 	const handleLogout = () => {
 		localStorage.removeItem("token")
 		localStorage.removeItem("UserDetails");
 		localStorage.removeItem("type")
 	};
-
+	const location = useLocation();
 	return (
 		<nav className='navbar navbar-expand-md navbar-dark bg-dark mb-4'>
 			<div className='container-fluid'>
 				<Link to='/' className='navbar-brand mr-auto'>
 					Home
 				</Link>
-				{localStorage.getItem("type") == "Candidate" ? (
+				{localStorage.getItem("type") === "Candidate" ? (
 					<>
 						<Link className='navbar-brand nav-link active' to='/jobsApplied'>
 							Applied Jobs
 						</Link>
 					</>
 				):(<></>)}
-				{localStorage.getItem("type") == "Company" ? (
+				{localStorage.getItem("type") === "Company" ? (
 					<>
 						<Link className='navbar-brand  active' to='/addJob'>
 							Add New Job
@@ -35,14 +35,22 @@ const NavBar = () => {
 					<ul className='navbar-nav me-auto mb-3 mb-md-0'>
 						<li className='nav-item'>
 							{localStorage.getItem("token") == null ? (
-								<>
+								 location.pathname !== "/login" ? (
 									<Link
 										to='/login'
 										className='nav-link active'
 										aria-current='page'>
 										LOGIN
 									</Link>
-								</>
+									): (
+										
+										<Link
+										to='/'
+										className='nav-link active'
+										aria-current='page'>
+										SIGN UP
+									</Link>
+									)
 							) : (
 								<>
 									<span className='nav-link active'>
